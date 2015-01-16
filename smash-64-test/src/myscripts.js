4b8e35111c90094e4d9fdc74831e6d7ec74a1893
G1Marquez <georgemarquez@outlook.com>
2015-01-15 23:27:11 -0500

    files

---
+++ smash-64-test/src/myscripts.js
@@ -0,0 +1,25 @@
+console.log('hello world')
+
+var characters = [
+'Mario',
+'Luigi',
+'Donkey Kong',
+'Link',
+'Samus',
+'Captain Falcon',
+'Ness',
+'Yoshi',
+'Kirby',
+'Fox',
+'Pikachu',
+'JigglyPuff'
+]
+
+console.log(characters)
+
+for ( var i = 0; i < characters.length; i++) { 
+    characters[i];
+}
+
+var x = document.getElementById("characters");
+
